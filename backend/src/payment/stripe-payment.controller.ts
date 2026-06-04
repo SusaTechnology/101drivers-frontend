5e4a65926e47c8fff5902fd0c92fbfbe28ef29c2
Z User <z@container>
2026-06-04 14:29:09 +0000

    fix: use AUTHORIZED instead of PENDING for payment status enum

--- backend/src/payment/stripe-payment.controller.ts
+++ backend/src/payment/stripe-payment.controller.ts
@@ -87,7 +87,7 @@ export class StripePaymentController {
             provider: "STRIPE",
             providerPaymentIntentId: result.paymentIntentId,
             paymentType: "PREPAID",
-            status: "PENDING",
+            status: "AUTHORIZED",
           },
         });
       }
